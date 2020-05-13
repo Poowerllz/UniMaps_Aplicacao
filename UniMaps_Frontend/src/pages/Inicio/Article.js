31059341f836b1d3cc0640d3e0c586623cfc252d
Elzevir Sá <safilhoelzevirsafilho@gmai.com>
2020-05-12 22:25:57 -0300

    FirstCommit

---
+++ UniMaps_Frontend/src/pages/Inicio/Article.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Jumbotron, 
+          Container,
+           } from 'reactstrap';
+const Article = (props) => {
+  return (
+    <div>      
+      <Jumbotron fluid>
+        <Container fluid>
+          <lista className="Lista">
+                  <h6>Filtros</h6>
+                  <select name="select" > 
+                      <option value="valor1" selected>Selecione</option>
+                      <option value="valor2">Entrada</option> 
+                      <option value="valor3">Refeitório</option>
+                      <option value="valor4">Biblioteca</option>
+                      <option value="valor5">Naaf</option>
+                      <option value="valor6">Prédio A</option>
+                  </select>
+                  <p></p>
+
+                  <h6>Local de partida</h6>
+                  <select name="select"> 
+                      <option value="valor1" selected>Selecione</option>
+                      <option value="valor2">Entrada</option> 
+                      <option value="valor3">Refeitório</option>
+                      <option value="valor4">Biblioteca</option>
+                      <option value="valor5">Naaf</option>
+                      <option value="valor6">Prédio A</option>
+                  </select>
+                  <p></p>
+                  
+
+                  <h6>Local de destino</h6>
+                  <select name="select"> 
+                      <option value="valor1" selected>Selecione</option>
+                      <option value="valor6">Prédio A</option>
+                      <option value="valor5">Naaf</option>
+                      <option value="valor4">Biblioteca</option>
+                      <option value="valor3">Refeitório</option>
+                      <option value="valor2">Entrada</option> 
+                  </select>
+                  </lista>
+        </Container>
+      </Jumbotron>
+
+    </div>
+  );
+};
+
+export default Article;
